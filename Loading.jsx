731a8149c36e59540c7744c3305fb1394d88c142
Andre Foresti <AF185076@ncr.com>
2021-12-06 16:17:52 -0300

    ajustes

--- Loading.jsx
+++ Loading.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import { RequestContext } from "../context/RequestContext";
+import { usePromiseTracker } from "react-promise-tracker";
 import { Backdrop, CircularProgress } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        "z-index": 2000,
-    },
-    progress: {
-        color: theme.palette.secondary.dark,
-    },
+const useStyles = makeStyles(theme => ({
+  root: {
+    "z-index": 2000,
+  },
+  progress: {
+    color: theme.palette.secondary.dark,
+  },
 }));
 
 export default function Loading() {
-    const { request } = React.useContext(RequestContext);
-    const classes = useStyles();
-    return (
-        <Backdrop open={request > 0} className={classes.root}>
-            <CircularProgress className={classes.progress} />
-        </Backdrop>
-    );
+  const { promiseInProgress } = usePromiseTracker();
+  const classes = useStyles();
+  return (
+    <Backdrop open={promiseInProgress} className={classes.root}>
+      <CircularProgress className={classes.progress} />
+    </Backdrop>
+  );
 }
