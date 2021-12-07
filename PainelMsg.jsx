c7ec7356ecb1eb3dac848062c80c508a9b9c6113
Andre Foresti <AF185076@ncr.com>
2021-12-07 12:49:18 -0300

    painel de mensagem movido para a lib

---
+++ PainelMsg.jsx
@@ -0,0 +1,31 @@
+import { Paper, Typography } from "@mui/material";
+import { Error, CheckCircleRounded } from "@mui/icons-material";
+import React from "react";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+    msgContainer: {
+        display: "flex",
+        padding: "20px 40px 20px 40px",
+        "align-items": "center",
+        "& svg": {
+            "margin-right": theme.spacing(1),
+        },
+    },
+}));
+
+export default function PainelMsg(props) {
+    const classes = useStyles();
+    return (
+        <Paper className={classes.msgContainer}>
+            {props.erro ? (
+                <Error fontSize="large" color="error" />
+            ) : (
+                <CheckCircleRounded fontSize="large" color="primary" />
+            )}
+            <Typography variant="subtitle1" color={props.erro ? "error" : "default"}>
+                {props.msg}
+            </Typography>
+        </Paper>
+    );
+}
