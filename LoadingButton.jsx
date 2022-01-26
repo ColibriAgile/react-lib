9d8e54e1fb1f6cafba2582b90da85b992e480986
Andre Foresti <AF185076@ncr.com>
2022-01-26 10:41:46 -0300

    formatação

--- LoadingButton.jsx
+++ LoadingButton.jsx
@@ -1,14 +1,15 @@
+import React from "react";
 import { Button, CircularProgress } from "@mui/material";
 import { usePromiseTracker } from "react-promise-tracker";
 
-const LoadingButton = (props) => {
-    const { promiseInProgress } = usePromiseTracker();
+const LoadingButton = props => {
+  const { promiseInProgress } = usePromiseTracker();
 
-    return (
-        <Button {...props} disabled={promiseInProgress}>
-            {promiseInProgress ? <CircularProgress size={22} /> : props.children}
-        </Button>
-    );
+  return (
+    <Button {...props} disabled={promiseInProgress}>
+      {promiseInProgress ? <CircularProgress size={22} /> : props.children}
+    </Button>
+  );
 };
 
 export default LoadingButton;
