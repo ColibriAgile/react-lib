a6d39f4209cce2d3fb70c3a603539a4ed35ce221
Andre Foresti <AF185076@ncr.com>
2022-01-27 13:19:07 -0300

    só mostra o botão se tiver um action disponivel

--- GridTitle.jsx
+++ GridTitle.jsx
@@ -1,34 +1,35 @@
 import React from "react";
 import TituloPagina from "./TituloPagina";
-import { useMediaQuery, Grid } from "@mui/material";
+import {useMediaQuery, Grid} from "@mui/material";
 import SplitButton from "./SplitButton";
 import ActionButton from "./ActionButton";
 
-export default function GridTitle({ titulo, autoFocus = true, actions }) {
-  const isMobile = useMediaQuery(theme => theme.breakpoints.down("sm"));
-  return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="flex-start"
-      alignItems="flex-start"
-    >
-      <TituloPagina titulo={titulo} />
-      {!Array.isArray(actions) ? (
-        <ActionButton action={actions} />
-      ) : isMobile ? (
-        <SplitButton options={actions} />
-      ) : (
-        actions.map((a, i) => (
-          <ActionButton
-            key={i}
-            action={a.action}
-            label={a.label}
-            color={a.color}
-            autoFocus={autoFocus && i === 0}
-          />
-        ))
-      )}
-    </Grid>
-  );
+export default function GridTitle({titulo, autoFocus = true, actions}) {
+    const isMobile = useMediaQuery(theme => theme.breakpoints.down("sm"));
+    return (
+        <Grid
+            container
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+        >
+            <TituloPagina titulo={titulo}/>
+            {actions && (
+                !Array.isArray(actions) ? (
+                    <ActionButton action={actions}/>
+                ) : isMobile ? (
+                    <SplitButton options={actions}/>
+                ) : (
+                    actions.map((a, i) => (
+                        <ActionButton
+                            key={i}
+                            action={a.action}
+                            label={a.label}
+                            color={a.color}
+                            autoFocus={autoFocus && i === 0}
+                        />
+                    ))
+                ))}
+        </Grid>
+    );
 }
