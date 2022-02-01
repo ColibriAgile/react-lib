cba1d489561aeb47ed11aad06b888cfe7fabd70b
Luis Fernando Attarian <luis.attarian@ncr.com>
2022-02-01 15:11:59 -0300

    +upload na api

--- Api.js
+++ Api.js
@@ -1,74 +1,76 @@
-import { trackPromise } from "react-promise-tracker";
+import {trackPromise} from "react-promise-tracker";
 
 const isErro = (res) => {
-  return res && !res.loading && res.status && [200, 201, 202, 204].indexOf(res.status) === -1;
+    return res && !res.loading && res.status && [200, 201, 202, 204].indexOf(res.status) === -1;
 };
 
 const isSucesso = (res) => {
-  return res && !res.loading && res.status && [200, 201, 202, 204].indexOf(res.status) > -1;
+    return res && !res.loading && res.status && [200, 201, 202, 204].indexOf(res.status) > -1;
 };
 
 const isLoading = (res) => {
-  return res && res.loading;
+    return res && res.loading;
 };
 
 const execRestRequest = async (client, request, loading) => {
-  const doRequest = async (request) => {
-    try {
-      return await client(request);
-    } catch (error) {
-      console.error(error);
-      if (error?.response?.status === 401) {
-        window.location.href = "/";
-      }
-      return error.response;
-    }
-  };
+    const doRequest = async (request) => {
+        try {
+            return await client(request);
+        } catch (error) {
+            console.error(error);
+            if (error?.response?.status === 401) {
+                window.location.href = "/";
+            }
+            return error.response;
+        }
+    };
 
-  if (loading) {
-    return trackPromise(doRequest(request));
-  }
-  return doRequest(request);
+    if (loading) {
+        return trackPromise(doRequest(request));
+    }
+    return doRequest(request);
 };
 
 const get = async (client, url, loading = true) => {
-  return execRestRequest(client,{ url, method: "get" }, loading);
+    return execRestRequest(client, {url, method: "get"}, loading);
 };
 
 const del = (client, url, loading = true) => {
-  return execRestRequest(client,{ url, method: "delete" }, loading);
+    return execRestRequest(client, {url, method: "delete"}, loading);
 };
 
 const post = (client, url, data, loading = true) => {
-  return execRestRequest(client,{ url, method: "post", data }, loading);
+    return execRestRequest(client, {url, method: "post", data}, loading);
 };
 
 const put = (client, url, data, loading = true) => {
-  return execRestRequest(client,{ url, method: "put", data }, loading);
+    return execRestRequest(client, {url, method: "put", data}, loading);
 };
 
-const uploadBlob = (url, data, loading = true) => {
-    return execRestRequest({
-        url,
-        method: "post",
-        data,
-        headers: {
-            "Content-Type": `blob.type; boundary=${data._boundary}`,
-        }
-    }, loading);
+const uploadBlob = (client, url, data, loading = true) => {
+    return execRestRequest(client,
+        {
+            url,
+            method: "post",
+            data,
+            headers: {
+                "Content-Type": `blob.type; boundary=${data._boundary}`,
+            }
+        }, loading);
 }
 
-const uploadFile = (url, data, loading = true, progress = null) => {
-    return execRestRequest({
-        url,
-        method: "post",
-        data,
-        headers: {
-            "Content-Type": "multipart/form-data"
-        },
-        onUploadProgress: progress
-    }, loading);
+const uploadFile = (client, url, data, loading = true, progress = null) => {
+    return execRestRequest(client,
+        {
+            url,
+            method: "post",
+            data,
+            headers: {
+                "Content-Type": "multipart/form-data"
+            },
+            onUploadProgress: progress
+        }, loading);
 }
 
 
-export { isErro, isSucesso, isLoading, get, del, post, put, uploadFile, uploadBlob, execRestRequest };
+export {isErro, isSucesso, isLoading, get, del, post, put, uploadFile, uploadBlob, execRestRequest};
