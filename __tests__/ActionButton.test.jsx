75300f0c01a1684d7affb902ad7453ff3116064d
Andre Foresti <AF185076@ncr.com>
2022-02-14 09:11:10 -0300

    ajuste no teste do ActionButton

--- __tests__/ActionButton.test.jsx
+++ __tests__/ActionButton.test.jsx
@@ -5,11 +5,22 @@ import ActionButton from "../ActionButton";
 import {ThemeProvider} from "@mui/material";
 import theme from "../../@nui/mui-theme";
 
-test('renders action button', () => {
+test('Deve renderizar action button', () => {
   render(
       <ThemeProvider theme={theme}>
         <ActionButton label="action" />
       </ThemeProvider>);
   const linkElement = screen.getByText(/action/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+test('Deve renderizar SpeedDialIcon se o label não for informado', () => {
+  render(
+      <ThemeProvider theme={theme}>
+        <ActionButton data-testid="action"/>
+      </ThemeProvider>);
+  const button = screen.getByRole("button");
+  const icon = screen.getByTestId('AddIcon')
+  expect(button).toContainElement(icon);
 });
