3ff05b635bb0b1fb7c2a3b26af9207e15972773e
Andre Foresti <AF185076@ncr.com>
2022-02-18 16:32:47 -0300

    removido socketApi da lib pra não criar dependencias indesejadas

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ export { useForm } from './hooks/useForm';
 export { useDimensions } from './hooks/useDimensions';
 export { useTableHeight, useTableHeightModal } from './hooks/useTableHeight';
 export { useNotificacao, NotificacaoProvider, NotificacaoContext, Alerta } from './context/NotificacaoContext';
-export { useSocketAPI } from './hooks/useSocketAPI';
