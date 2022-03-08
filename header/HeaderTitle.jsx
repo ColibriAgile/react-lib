076685026d4eaa635de91506d68a668f273b75e4
Andre Foresti <AF185076@ncr.com>
2022-03-08 14:49:03 -0300

    mais um ajuste no título

--- header/HeaderTitle.jsx
+++ header/HeaderTitle.jsx
@@ -1,30 +1,30 @@
 import React from "react";
-import {Typography, useMediaQuery} from "@mui/material";
-import {makeStyles, useTheme} from "@mui/styles";
+import { Typography, useMediaQuery } from "@mui/material";
+import { makeStyles, useTheme } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
   title: {
-      marginLeft: theme.spacing(2),
-      marginRight: theme.spacing(2),
-      marginTop: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
   },
   version: {
-      marginTop: '-1px',
-      fontSize: "0.7rem"
+    position: "absolute",
+    bottom: theme.spacing(0.5),
+    fontSize: "0.7rem",
   },
 }));
 
-export default function HeaderTitle({title, version}) {
+export default function HeaderTitle({ title, version }) {
   const classes = useStyles();
   const theme = useTheme();
   const mobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
-      <div className={classes.title}>
-        <Typography variant={mobile ? "body2" : "body1"}>{title}</Typography>
-        <Typography variant={"body2"} className={classes.version}>
-          {version}
-        </Typography>
-      </div>
+    <div className={classes.title}>
+      <Typography variant={mobile ? "body2" : "body1"}>{title}</Typography>
+      <Typography variant={"body2"} className={classes.version}>
+        {version}
+      </Typography>
+    </div>
   );
 }
