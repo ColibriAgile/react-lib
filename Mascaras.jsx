5c2d59cc3b871351a96f9b7738f41c1b74e73567
Andre Foresti <AF185076@ncr.com>
2022-05-31 16:26:53 -0300

    + Mascaras usando iMask

---
+++ Mascaras.jsx
@@ -0,0 +1,51 @@
+import React, { forwardRef } from "react";
+import {IMaskInput} from "react-imask";
+
+
+const MascaraTelefone = forwardRef((props, ref) => {
+    const {onChange, ...other} = props;
+    return (
+        <IMaskInput
+            {...other}
+            mask="(#0) 00000-0000"
+            definitions={{
+                '#': /[1-9]/,
+            }}
+            inputRef={ref}
+            onAccept={(value) => onChange({target: {name: props.name, value}})}
+            overwrite
+        />
+    );
+});
+
+const MascaraCep = forwardRef((props, ref) => {
+    const {onChange, ...other} = props;
+    return (
+        <IMaskInput
+            {...other}
+            mask="00000-000"
+            inputRef={ref}
+            onAccept={(value) => onChange({target: {name: props.name, value}})}
+            overwrite
+        />
+    );
+});
+
+const MascaraCnpj = forwardRef((props, ref) => {
+    const {onChange, ...other} = props;
+    return (
+        <IMaskInput
+            {...other}
+            mask="00.000.000.000/0000-00"
+            inputRef={ref}
+            onAccept={(value) => onChange({target: {name: props.name, value}})}
+            overwrite
+        />
+    );
+});
+
+function desmascararNumero(valor) {
+    return valor.replace(/\D+/g, "");
+}
+
+export { MascaraTelefone, MascaraCep, MascaraCnpj, desmascararNumero };
