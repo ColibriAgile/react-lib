a1589fef1c6c23dc5b58cd1e7328b065eed9f849
Andre Foresti <AF185076@ncr.com>
2022-06-06 09:48:48 -0300

    ajuste na mascara do cnpj

--- Mascaras.jsx
+++ Mascaras.jsx
@@ -36,7 +36,7 @@ const MascaraCnpj = forwardRef((props, ref) => {
     return (
         <IMaskInput
             {...other}
-            mask="00.000.000.000/0000-00"
+            mask="00.000.000/0000-00"
             inputRef={ref}
             onAccept={(value) => onChange({target: {name: props.name, value}})}
             overwrite
