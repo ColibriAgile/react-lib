92b01923bd27bd4171490103fde7a17c4935ed25
Luis Fernando Attarian <luis.attarian@ncr.com>
2023-01-26 16:53:31 -0300

    main - novos componentes

---
+++ PasswordField.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import {IconButton} from "@mui/material";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+import TextField from "@mui/material/TextField";
+
+export default function PasswordField(props) {
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+        <TextField
+            {...props}
+            type={showPassword ? 'text' : 'password'}
+            InputProps={{
+                ...props.InputProps,
+                endAdornment:
+                    <IconButton
+                        aria-label="toggle password visibility"
+                        onMouseDown={() => setShowPassword(true)}
+                        onMouseUp={() => setShowPassword(false)}
+                        onMouseLeave={() => setShowPassword(false)}
+                        edge="end"
+                    >
+                        {showPassword ? <VisibilityOff/> : <Visibility/>}
+                    </IconButton>
+            }}
+        />
+    );
+}
