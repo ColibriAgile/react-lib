63495a2daf02e6e7ce1b2acb48d7ebb9cf28c807
Luis Fernando Attarian <luis.attarian@ncr.com>
2023-06-07 14:33:00 -0300

    main - suporte a esconder botoes de acao no modal

--- Modal.jsx
+++ Modal.jsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useImperativeHandle } from "react";
+import React, {forwardRef, useImperativeHandle} from "react";
 import {
     Dialog,
     DialogContent,
@@ -7,10 +7,10 @@ import {
     useMediaQuery, useTheme, styled,
 } from "@mui/material";
 import ModalTitulo from "./ModalTitulo";
-import { useTranslation } from "react-i18next";
+import {useTranslation} from "react-i18next";
 import LoadingButton from "./LoadingButton";
 
-const DialogActionsContainer = styled(DialogActions)(({ theme }) => ({
+const DialogActionsContainer = styled(DialogActions)(({theme}) => ({
     [theme.breakpoints.down("sm")]: {
         justifyContent: "flex-start",
         "& button": {
@@ -21,86 +21,91 @@ const DialogActionsContainer = styled(DialogActions)(({ theme }) => ({
 
 
 const Modal = forwardRef(
-  (
-    {
-      onClose,
-      onEntered,
-      fullWidth,
-      maxWidth,
-      onExited,
-      titulo,
-      onConfirm,
-      actionName,
-      children,
-      autoFocus = false,
-      fullScreen = false,
-    },
-    ref
-  ) => {
-    const { t } = useTranslation();
-    const theme = useTheme();
-    const full = useMediaQuery(theme.breakpoints.down("sm"));
-    const [open, setOpen] = React.useState(false);
+    (
+        {
+            onClose,
+            onEntered,
+            fullWidth,
+            maxWidth,
+            onExited,
+            titulo,
+            onConfirm,
+            actionName,
+            children,
+            autoFocus = false,
+            fullScreen = false,
+            hideActions = false
+        },
+        ref
+    ) => {
+        const {t} = useTranslation();
+        const theme = useTheme();
+        const full = useMediaQuery(theme.breakpoints.down("sm"));
+        const [open, setOpen] = React.useState(false);
 
-    const handleClose = item => {
-      setOpen(false);
-      if (onClose) {
-        onClose(item);
-      }
-    };
+        const handleClose = item => {
+            setOpen(false);
+            if (onClose) {
+                onClose(item);
+            }
+        };
 
-    const handleOpen = () => {
-      setOpen(true);
-    };
+        const handleOpen = () => {
+            setOpen(true);
+        };
 
-    useImperativeHandle(ref, () => ({
-      openModal() {
-        handleOpen();
-      },
-      closeModal(item) {
-        handleClose(item);
-      },
-    }));
+        useImperativeHandle(ref, () => ({
+            openModal() {
+                handleOpen();
+            },
+            closeModal(item) {
+                handleClose(item);
+            },
+        }));
 
-    return (
-      <Dialog
-        fullScreen={fullScreen || full}
-        open={open}
-        fullWidth={fullWidth || full}
-        maxWidth={maxWidth}
-        scroll="body"
-        TransitionProps={{
-          onEntered: onEntered,
-          onExited: onExited,
-        }}
-        onClose={(event, reason) => {
-          if (reason === "escapeKeyDown") {
-            handleClose();
-          }
-        }}
-      >
-        <ModalTitulo titulo={titulo} onClose={() => handleClose()} />
-        <DialogContent dividers>{children}</DialogContent>
-        <DialogActionsContainer>
-          <LoadingButton
-            variant="contained"
-            onClick={onConfirm}
-            color="primary"
-            autoFocus={autoFocus}
-          >
-            {actionName ?? t("acao.ok")}
-          </LoadingButton>
-          <Button
-            variant="outlined"
-            onClick={() => handleClose()}
-            color="primary"
-          >
-            {t("acao.cancelar")}
-          </Button>
-        </DialogActionsContainer>
-      </Dialog>
-    );
-  }
+        return (
+            <Dialog
+                fullScreen={fullScreen || full}
+                open={open}
+                fullWidth={fullWidth || full}
+                maxWidth={maxWidth}
+                scroll="body"
+                TransitionProps={{
+                    onEntered: onEntered,
+                    onExited: onExited,
+                }}
+                onClose={(event, reason) => {
+                    if (reason === "escapeKeyDown") {
+                        handleClose();
+                    }
+                }}
+            >
+                <ModalTitulo titulo={titulo} onClose={() => handleClose()}/>
+                <DialogContent dividers>{children}</DialogContent>
+                {!hideActions &&
+                    (
+                        <DialogActionsContainer>
+                            <LoadingButton
+                                variant="contained"
+                                onClick={onConfirm}
+                                color="primary"
+                                autoFocus={autoFocus}
+                            >
+                                {actionName ?? t("acao.ok")}
+                            </LoadingButton>
+                            <Button
+                                variant="outlined"
+                                onClick={() => handleClose()}
+                                color="primary"
+                            >
+                                {t("acao.cancelar")}
+                            </Button>
+                        </DialogActionsContainer>
+                    )
+                }
+            </Dialog>
+        );
+    }
 );
 
 export default Modal;
