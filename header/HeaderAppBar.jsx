a22d79fbdcbdc72b55064c20fd765cb1e1b74965
Luis Fernando Attarian <luis.attarian@ncr.com>
2023-11-17 11:52:59 -0300

    main - cores e logo voyix

--- header/HeaderAppBar.jsx
+++ header/HeaderAppBar.jsx
@@ -1,30 +1,35 @@
 import React from "react";
 import {AppBar, Box, styled, Toolbar, useMediaQuery, useTheme} from "@mui/material";
-import logoNCR from "../img/ncr-logo-handshake.svg";
+import logoVoyix from "../img/ncr-voyix-logo.svg";
 import {useNavigate} from "react-router-dom";
 
 const AppBarNcr = styled(AppBar)(({ theme }) => ({
     flexGrow: 1,
     border: "none",
-    backgroundColor: "#18565e",
+    backgroundColor: theme.palette.primary.main,
     color: theme.palette.common.white,
 }));
 
+
 const ImgLogo = styled('img')({
     cursor: "pointer"
-})
+});
 
 
 export default function HeaderAppBar({children}) {
     const navigate = useNavigate();
     const theme = useTheme();
     const mobile = useMediaQuery(theme.breakpoints.down("sm"));
+    console.log(theme.palette);
+    console.log(theme.palette);
 
     return (
         <Box sx={{flexGrow: 1, marginBottom: theme.spacing(10)}}>
             <AppBarNcr>
                 <Toolbar disableGutters>
-                    {!mobile && <ImgLogo src={logoNCR}  alt="NCR" height="64" width="64" onClick={() => navigate("/")}/>}
+                    {!mobile &&
+                        <ImgLogo src={logoVoyix}  alt="NCR" width="120" onClick={() => navigate("/")}/>
+                    }
                     {children}
                 </Toolbar>
             </AppBarNcr>
